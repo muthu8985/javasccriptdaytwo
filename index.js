6d9d9d20039c92dacc24262b60971989d9d4fcd9
Muthu Kumar <muthukumarv8985@gmail.com>
2024-10-01 13:56:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// declaring variables types 1.var& arithmetic operators (+ addition)
+var x=1
+ var y=2
+ var z=x+y
+ document.write(z)
+ 
+//  declaring values type 2 let (-Subtraction)
+ let a =11
+ let b=12
+ let c =b-a
+ document.write(c)
+
+//  declaring values type 3 const (Multiplication)
+ const d=12
+ const e=14
+ const f=d*e
+ document.write(f)
+
+ 
